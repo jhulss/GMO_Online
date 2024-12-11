0cc10d9027f88edc33fcc89c25c672c3c59e8faf
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2024-12-10 21:06:19 -0400

    eliminacion de Ptravel innecesario de env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,11 +3,7 @@ begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations
     require 'capybara/dsl'
     require 'capybara/cucumber'
     require 'capybara-screenshot/cucumber'
-    
-    #PTravel Settings
-    ENV['USER']="Pepazo"
-    ENV['PSW']="ILoveQA"
-    
+
     Capybara.default_driver = :selenium
     
     # Set the host the Capybara tests should be run against
